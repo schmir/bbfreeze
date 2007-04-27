5b6ae828358eafbf1bbbfe86149fd8e786455a35
ralf@brainbot.com <ralf@brainbot.com>
2007-04-27 12:54:36 +0200

    actually, we have enough changes to up the minor version. this is 0.92.0.

--- setup.py
+++ setup.py
@@ -153,7 +153,7 @@ if sys.platform=='win32':
 setup(name = "bbfreeze",
       cmdclass         = {'build_ext': BuildInterpreters,
                           },
-      version = '0.91.1',
+      version = '0.92.0',
       entry_points = dict(console_scripts=['bb-freeze = bbfreeze:main']),
       ext_modules = ext_modules,
       install_requires=install_requires,
