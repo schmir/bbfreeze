927467a40d88797e7cea2d658995779754c2d0d3
ralf@brainbot.com <ralf@brainbot.com>
2007-05-29 13:26:35 +0200

    handle shared libraries with modulegraph

--- bbfreeze/freezer.py
+++ bbfreeze/freezer.py
@@ -48,6 +48,12 @@ def getRecipes():
 
     return res
 
+class SharedLibrary(modulegraph.Node):
+    def __init__(self, identifier):
+        self.graphident = identifier
+        self.identifier = identifier
+        self.filename = None        
+        
 class ZipModule(modulegraph.BaseModule):
     pass
 
@@ -228,6 +234,17 @@ class Freezer(object):
         for x in getRecipes():
             if x(self.mf):
                 print "*** applied", x
+
+    def findBinaryDependencies(self):
+        from bbfreeze import getdeps
+
+        for x in list(self.mf.flatten()):
+            if isinstance(x, modulegraph.Extension):
+                for so in getdeps.getDependencies(x.filename):
+                    n = self.mf.createNode(SharedLibrary, os.path.basename(so))
+                    n.filename = so
+                    self.mf.createReference(x, n)
+                
         
     def __call__(self):
         if self.include_py:
@@ -238,6 +255,8 @@ class Freezer(object):
         self._add_loader()
         self._handleRecipes()
 
+        self.findBinaryDependencies()
+        
         if os.path.exists(self.distdir):
             shutil.rmtree(self.distdir)
         os.makedirs(self.distdir)
@@ -269,7 +288,7 @@ class Freezer(object):
         self.outfile.close()
         # be sure to close the file before scanning for binary dependencies
         # otherwise ldd might not be able to do it's job ("Text file busy")
-        self.copyBinaryDependencies()
+        #self.copyBinaryDependencies()
 
         if os.environ.get("XREF") or os.environ.get("xref"):
             self.showxref()
@@ -385,16 +404,14 @@ class Freezer(object):
             return
         os.environ['S'] = p
         os.system('strip $S')
+
+    def _handle_SharedLibrary(self, m):
+        dst = os.path.join(self.distdir, os.path.basename(m.filename))
+        shutil.copy2(m.filename, dst)
+        os.chmod(dst, 0755)
+        self.stripBinary(dst)
         
-    def copyBinaryDependencies(self):
-        from bbfreeze import getdeps
         
-        for x in getdeps.getDependencies(self.binaries):
-            dst = os.path.join(self.distdir, os.path.basename(x))
-            shutil.copy2(x, dst)
-            os.chmod(dst, 0755)
-            self.stripBinary(dst)
-                
     def showxref(self):
         import tempfile
         import urllib
