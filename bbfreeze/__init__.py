fe4311cd5489dd09e287704010acea2c2c65dc9e
ralf@brainbot.com <ralf@brainbot.com>
2007-05-30 17:39:15 +0200

    up version to 0.93.2

--- bbfreeze/__init__.py
+++ bbfreeze/__init__.py
@@ -5,7 +5,7 @@ class _Version(tuple):
     def __str__(self):
         return '.'.join([str(x) for x in self])
 
-version = _Version((0,93,2,'dev'))
+version = _Version((0,93,2))
 del _Version
 from bbfreeze.freezer import Freezer
 
