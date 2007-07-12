f098c189756deb7da5ac93462f318b8b437850dd
ralf@brainbot.com <ralf@brainbot.com>
2007-07-12 09:56:16 +0200

    remove make_manifest.py, .hgtags

--- make_manifest.py
+++ make_manifest.py
@@ -5,13 +5,14 @@ import os
 def main():
     files = [x.strip() for x in os.popen("hg manifest")]
     files.append("README.html")
+    
+    files.remove("make_manifest.py")
+    files.remove(".hgtags")
 
     files.sort()
 
     f = open("MANIFEST.in", "w")
     for x in files:
-        if x==".hgtags":
-            continue
         f.write("include %s\n" % x)
     f.close()
 
