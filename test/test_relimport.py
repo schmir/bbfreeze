afc78aa8eef4f915e94032262951f1ad31ecb6c4
ralf@brainbot.com <ralf@brainbot.com>
2008-03-13 10:27:43 +0100

    test relative imports only with python >= 2.5

--- test/test_relimport.py
+++ test/test_relimport.py
@@ -1,9 +1,12 @@
 #! /usr/bin/env py.test
 
-def test_freeze_relimport():
-    from bbfreeze import Freezer
-    f=Freezer(includes = ['relimport'])
-    f()
+import sys
+
+if sys.version_info >= (2,5):
+    def test_freeze_relimport():
+        from bbfreeze import Freezer
+        f=Freezer(includes = ['relimport'])
+        f()
 
 if __name__=='__main__':
     test_freeze_relimport()
