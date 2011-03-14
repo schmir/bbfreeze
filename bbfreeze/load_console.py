4b2c83b8cd42cbf014f81b5cba542c39e44ca530
Ralf Schmitt <ralf@systemexit.de>
2011-03-14 23:24:50 +0100

    load_console.py: whitespace cleanup.

--- bbfreeze/load_console.py
+++ bbfreeze/load_console.py
@@ -1,11 +1,9 @@
 #! /usr/bin/env python
 
-import sys
-import os
-import zlib
-import zipimport
+import sys, os, zlib, zipimport
 
-installdir = os.path.normpath(os.path.dirname(sys.path[0])) # sys.path[0]=='.../library.zip'
+installdir = os.path.normpath(os.path.dirname(sys.path[0]))  # sys.path[0] == '.../library.zip'
+
 
 def find_eggs():
     for x in os.listdir(installdir):
@@ -15,13 +13,14 @@ def find_eggs():
 
 find_eggs()
 
+
 def addldlibrarypath():
 
-    if sys.platform=='darwin':
-        LD_LIBRARY_PATH='DYLD_LIBRARY_PATH'
+    if sys.platform == 'darwin':
+        LD_LIBRARY_PATH = 'DYLD_LIBRARY_PATH'
     else:
-        LD_LIBRARY_PATH='LD_LIBRARY_PATH'
-        
+        LD_LIBRARY_PATH = 'LD_LIBRARY_PATH'
+
     #p = os.path.normpath(os.path.dirname(sys.executable))
     p = installdir
     try:
@@ -30,11 +29,12 @@ def addldlibrarypath():
         paths = []
 
     if p not in paths:
-        paths.insert(0,p)
+        paths.insert(0, p)
         os.environ[LD_LIBRARY_PATH] = os.pathsep.join(paths)
         #print "SETTING", LD_LIBRARY_PATH, os.environ[LD_LIBRARY_PATH]
         os.execv(sys.executable, sys.argv)
-    
+
+
 def addpath():
     # p = os.path.normpath(os.path.dirname(sys.executable))
     p = installdir
@@ -49,21 +49,21 @@ def addpath():
         #print "SETTING PATH:", os.environ['PATH']
 
 
-
 def addtcltk():
     libtk = os.path.join(installdir, "lib-tk")
     libtcl = os.path.join(installdir, "lib-tcl")
-    
+
     if os.path.isdir(libtk):
         os.environ['TK_LIBRARY'] = libtk
-        
+
     if os.path.isdir(libtcl):
         os.environ['TCL_LIBRARY'] = libtcl
+
 
 def fixwin32com():
     """setup win32com to 'genpy' in a tmp directory
     """
-    if sys.platform!='win32':
+    if sys.platform != 'win32':
         return
 
     # hide imports by using exec. bbfreeze analyzes this file.
@@ -92,7 +92,7 @@ addpath()
 #    addldlibrarypath()
 
 addtcltk()
-    
+
 try:
     import encodings
 except ImportError:
@@ -104,16 +104,16 @@ exe = os.path.basename(sys.argv[0])
 if exe.lower().endswith(".exe"):
     exe = exe[:-4]
 
-    
+
 m = __import__("__main__")
 
-if exe=='py' and len(sys.argv)==2:
+if exe == 'py' and len(sys.argv) == 2:
     del sys.argv[0]
     m.__dict__['__file__'] = sys.argv[0]
     exec open(sys.argv[0], 'r') in m.__dict__
 else:
     # add '.py' suffix to prevent garbage from the warnings module
-    m.__dict__['__file__'] = exe+'.py'
+    m.__dict__['__file__'] = exe + '.py'
     exe = exe.replace(".", "_")
     importer = zipimport.zipimporter(sys.path[0])
     while 1:
@@ -127,5 +127,5 @@ else:
                 raise err
         else:
             break
-        
+
     exec code in m.__dict__
