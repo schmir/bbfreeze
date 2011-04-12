e0edfeb6c0557e2851dc18a7c4b0f21c6953e618
Ralf Schmitt <ralf@systemexit.de>
2011-04-12 02:06:58 +0200

    _version.py: up version to 0.97.3

--- bbfreeze/_version.py
+++ bbfreeze/_version.py
@@ -1,9 +1,9 @@
 class _Version(tuple):
     """internal version object, subclass of C{tuple},
-    but implements a more nice __str__ representation
+    but implements a fancier __str__ representation
     """
     def __str__(self):
         return '.'.join([str(x) for x in self])
 
-version = _Version((0, 97, 2))
+version = _Version((0, 97, 3))
 del _Version
