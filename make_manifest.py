48a0fe765280c1ec1252c04e4dc83fa83197a4f1
Ralf Schmitt <ralf@systemexit.de>
2012-10-23 21:01:35 +0200

    shorten make_manifest.py

--- make_manifest.py
+++ make_manifest.py
@@ -4,14 +4,8 @@ import os
 
 
 def main():
-    files = [x.strip() for x in os.popen("git ls-files")]
-    files.append("README.html")
-
-    files.remove("make_manifest.py")
-    files.remove("Makefile")
-    files.remove(".gitignore")
-
-    files.sort()
+    files = sorted(set([x.strip() for x in os.popen("git ls-files")])
+                   - set(["make_manifest.py", "Makefile", ".gitignore"]))
 
     f = open("MANIFEST.in", "w")
     for x in files:
