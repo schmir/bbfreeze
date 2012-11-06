57d7b2cca007def76fce4e85b501e66439c0ac7b
Ralf Schmitt <ralf@systemexit.de>
2012-11-07 00:26:51 +0100

    make showvars.py work on python 3

--- showvars.py
+++ showvars.py
@@ -1,13 +1,14 @@
 #! /usr/bin/env python
 """show distutils's config variables"""
 
+import sys
+
 
 def main():
     import distutils.sysconfig
-    items = distutils.sysconfig.get_config_vars().items()
-    items.sort()
+    items = sorted(distutils.sysconfig.get_config_vars().items())
     for k, v in items:
-        print "%s: %r" % (k, v)
+        sys.stdout.write("%s: %r\n" % (k, v))
 
 if __name__ == '__main__':
     main()
